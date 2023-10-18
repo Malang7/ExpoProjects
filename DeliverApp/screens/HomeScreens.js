d91b46d7107e0d757398ec74c92c39ae2b972663
Matin Mohammadi <matinm@student.uia.no>
2023-10-18 15:41:02 +0200

    start with implementation of DeliverApp, setup the expo and tailwind css for react native

---
+++ DeliverApp/screens/HomeScreens.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+
+function HomeScreen() {
+    return (
+      <View className= 'text-blue-500'>
+        <Text>Home Screen homayoon</Text>
+      </View>
+    );
+  }
+
+
+
+  export default HomeScreen;
